c57b17577b1ae471fd1216f2e2da20d4f669717a
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2022-01-20 13:06:09 +0100

    Anpassungen

--- init.sql
+++ init.sql
@@ -1,12 +1,10 @@
-CREATE USER gitea WITH PASSWORD 'gitea';
-CREATE DATABASE gitea;
-GRANT ALL PRIVILEGES ON DATABASE gitea TO gitea;
-
 \connect project
 
 CREATE TABLE IF NOT EXISTS meta
 (
-    _id SERIAL,
-    data JSON NOT NULL,
-    PRIMARY KEY (_id)
+    id SERIAL,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    keywords varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
