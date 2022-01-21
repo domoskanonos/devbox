94e3f1b035a44f06cb20f51e945bf2638f189e17
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2022-01-21 22:19:31 +0100

    Anpassungen

--- init.sql
+++ init.sql
@@ -1,10 +1,8 @@
-\connect project
-
-CREATE TABLE IF NOT EXISTS meta
-(
+\ connect project CREATE TABLE IF NOT EXISTS meta (
     id SERIAL,
     name varchar(255) NOT NULL,
     description varchar(255) NOT NULL,
     keywords varchar(255) NOT NULL,
+    logo_id bigint,
     PRIMARY KEY (id)
 );
